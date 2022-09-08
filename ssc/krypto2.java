d3d944b619c0d123cffa63f8fc5c0acca7655e6a
albertsparrman <77719327+albertsparrman@users.noreply.github.com>
2022-09-08 09:58:34 +0200

    Det funkar!! så episkt

--- ssc/krypto2.java
+++ ssc/krypto2.java
@@ -1,12 +1,32 @@
 public class krypto2 {
     public static void main(String[] args) {
-        String str = "Bing Bong";
-        char[] key = {'r', 'i', 's', 'k', 'a', 'k', 'a'} ;
-        for (int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-        }
-        for (int e = key.length; e < str.length(); e += e){
+        String str = "BINGUS BONGUS";
+        String key = "fortnite";
+        String copy = key;
+        int wrdlgt = str.length();
+        char[] array = new char[wrdlgt];
+        char[] arr = new char[wrdlgt];
+
+        System.out.println(str);
+        for (int e = key.length(); e < str.length(); e+=e){
+            key += copy;
             System.out.println(key);
         }
+        for (int i = 0; i < str.length(); i++){
+            char ke = key.charAt(i);
+            char ch = str.charAt(i);
+            char beans = (char) (ch^ke);
+            array[i]= beans;
+        }
+        System.out.println(array);
+
+        for (int i = 0; i < str.length(); i++){
+            char ke = key.charAt(i);
+            char be = array[i];
+            char frid = (char) (be^ke);
+            arr[i]= frid;
+        }
+        System.out.println(arr);
+
     }
 }
